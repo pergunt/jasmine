fe7aa4ed9956c0a6ef203d467e8306e82b3ab92e
ivanFullStackSenior <ivan5243552@gmail.com>
2018-04-11 11:59:00 +0300

    finish second lesson

--- spec/computer_spec.js
+++ spec/computer_spec.js
@@ -1,23 +1,52 @@
-describe('Computer', function() {
+describe('Computer', () => {
     beforeEach(function() {
        computer.RAM = 2;
+       computer.spaceGBs = 1000;
     });
-    describe('When RAM is incremented', function() {
-        it('Should to have a possibility to increase his own RAM', function() {
+    describe('When RAM is incremented', () => {
+        it('Should to have a possibility to increase his own RAM', () => {
             expect(computer.addMemory(6)).toEqual(8)
         });
-        it('Should to show a current RAM', function() {
-            expect(computer.getCurrentMemory).toBeDefined();
-            expect(computer.getCurrentMemory).toEqual(2);
+        it('Should to show a current RAM', () => {
+            expect(computer.getRam).toBeDefined();
+            expect(computer.getRam).toEqual(2);
         });
-        it('Must to reject incrementing slot bigger then 2', function() {
+        it('Must to reject incrementing slot bigger then 2', () => {
             expect(computer.addMemory(6,8)).toEqual(16);
             expect(computer.addMemory(6,8,6)).toBe(false)
         })
     })
-    describe('when installing programs', function() {
-        it('must install count of programs', function() {
-            expect()
-        })
-    })
+    describe('when installing programs', () => {
+		const gta = {
+			space: 600,
+			installed: false,
+			name: 'gta'
+		};
+		const PS = {
+			space: 400,
+			installed: false,
+			name: 'PS'
+		};
+		const sublime = {
+			space: 1,
+			installed: false,
+			name: 'sublime'
+		};
+        it('must to install any count of programs', () => {
+            expect(computer.install(gta,PS)).toBe(true);
+        });
+		it('must to discard installation if not enough space', () => {
+			expect(computer.install(gta,PS,sublime)).toBe(false);
+		});
+    });
+
+    it('posibility for cleaning spaceGBs', () => {
+    	computer.install({
+			space: 600,
+			installed: false,
+			name: 'gta'
+		});
+    	computer.format();
+		expect(computer.getCurrentMemory).toEqual(1000)
+	})
 });
